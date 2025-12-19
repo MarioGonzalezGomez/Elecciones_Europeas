75f6e08a33db52450737d94674fd10a565c41510
Mario <ggperpi@gmail.com>
2025-12-19 10:37:09 +0100

    Codigo padre

--- src/mensajes/builders/IPFMensajes.cs
+++ src/mensajes/builders/IPFMensajes.cs
@@ -38,44 +38,94 @@ namespace Elecciones_Europeas.src.mensajes.builders
 
         //FALDONES
         //CAMBIO ENTRE OFI Y SONDEO
-        public string SondeoUOficial(bool oficiales) { return oficiales ? EventBuild("Sondeo_Oficiales", "MAP_INT_PAR", "1", 1) : EventBuild("Sondeo_Oficiales", "MAP_INT_PAR", "0", 1); }
+        public string SondeoUOficial(bool oficiales)
+        {
+            return oficiales ? EventBuild("Sondeo_Oficiales", "MAP_INT_PAR", "1", 1) : EventBuild("Sondeo_Oficiales", "MAP_INT_PAR", "0", 1);
+        }
 
         //ANIMACIONES
-        public string PrimerosResultados(bool activo) { return activo ? EventBuild("PRIMEROS", "MAP_INT_PAR", "1", 1) : EventBuild("PRIMEROS", "MAP_INT_PAR", "0", 1); }
-        public string AnimacionSondeo(bool activo) { return activo ? EventBuild("SONDEO", "MAP_INT_PAR", "1", 1) : EventBuild("SONDEO", "MAP_INT_PAR", "0", 1); }
+        public string PrimerosResultados(bool activo)
+        {
+            return activo ? EventBuild("PRIMEROS", "MAP_INT_PAR", "1", 1) : EventBuild("PRIMEROS", "MAP_INT_PAR", "0", 1);
+        }
+        public string AnimacionSondeo(bool activo)
+        {
+            return activo ? EventBuild("SONDEO", "MAP_INT_PAR", "1", 1) : EventBuild("SONDEO", "MAP_INT_PAR", "0", 1);
+        }
 
-        public string RecibirPrimerosResultados() { return EventBuild("PRIMEROS", "MAP_INT_PAR", 3); }
-        public string RecibirAnimacionSondeo() { return EventBuild("SONDEO", "MAP_INT_PAR", 3); }
+        public string RecibirPrimerosResultados()
+        {
+            return EventBuild("PRIMEROS", "MAP_INT_PAR", 3);
+        }
+        public string RecibirAnimacionSondeo()
+        {
+            return EventBuild("SONDEO", "MAP_INT_PAR", 3);
+        }
 
         //PROYECCION
-        public string Proyeccion(bool activo) { return activo ? EventBuild("TICKER/PROYECCION", "MAP_INT_PAR", "1", 1) : EventBuild("TICKER/PROYECCION", "MAP_INT_PAR", "0", 1); }
+        public string Proyeccion(bool activo)
+        {
+            return activo ? EventBuild("TICKER/PROYECCION", "MAP_INT_PAR", "1", 1) : EventBuild("TICKER/PROYECCION", "MAP_INT_PAR", "0", 1);
+        }
 
         //GIROS
-        public string DeSondeoAOficiales() { return EventRunBuild("SONDEOaOFICIALES"); }
+        public string DeSondeoAOficiales()
+        {
+            return EventRunBuild("SONDEOaOFICIALES");
+        }
 
         //CAMBIO DE ELECCIONES
-        public string CambioElecciones(bool europa) { return europa ? EventBuild("TICKER/EUROPA", "MAP_INT_PAR", "1", 1) : EventBuild("TICKER/EUROPA", "MAP_INT_PAR", "0", 1); }
+        public string CambioElecciones(bool europa)
+        {
+            return europa ? EventBuild("TICKER/EUROPA", "MAP_INT_PAR", "1", 1) : EventBuild("TICKER/EUROPA", "MAP_INT_PAR", "0", 1);
+        }
 
         //RELOJ
-        public string EntraReloj() { return EventRunBuild("EntraReloj"); }
-        public string SaleReloj() { return EventRunBuild("SaleReloj"); }
-        public string PreparaReloj(string time) { return EventBuild("OBJETO", "TEXT_STRING", time, 1); }
+        public string EntraReloj()
+        {
+            return EventRunBuild("EntraReloj");
+        }
+        public string SaleReloj()
+        {
+            return EventRunBuild("SaleReloj");
+        }
+        public string PreparaReloj(string time)
+        {
+            return EventBuild("OBJETO", "TEXT_STRING", time, 1);
+        }
 
         //TICKER
-        public string TickerEntra(bool oficial) { return oficial ? EventRunBuild("TICKER/ENTRA") : EventRunBuild("TICKER_SONDEO/ENTRA"); }
-        public string TickerEncadena(bool oficial) { return oficial ? Encadena("TICKER") : Encadena("TICKER_SONDEO"); }
-        public string TickerActualiza() { return EventRunBuild("TICKER/ACTUALIZO"); }
-        public string TickerSale(bool oficial) { return oficial ? EventRunBuild("TICKER/SALE") : EventRunBuild("TICKER_SONDEO/SALE"); }
+        public string TickerEntra(bool oficial)
+        {
+            return oficial ? EventRunBuild("TICKER/ENTRA") : EventRunBuild("TICKER_SONDEO/ENTRA");
+        }
+        public string TickerEncadena(bool oficial)
+        {
+            return oficial ? Encadena("TICKER") : Encadena("TICKER_SONDEO");
+        }
+        public string TickerActualiza()
+        {
+            return EventRunBuild("TICKER/ACTUALIZO");
+        }
+        public string TickerSale(bool oficial)
+        {
+            return oficial ? EventRunBuild("TICKER/SALE") : EventRunBuild("TICKER_SONDEO/SALE");
+        }
 
-        public string TickerActualizaEscrutado() { return EventBuild("TICKER/CambiaEscrutado", "MAP_INT_PAR", "1", 1); }
-        public string TickerActualizaDatos() { return EventBuild("TICKER/CambiaResultado", "MAP_INT_PAR", "1", 1); }
+        public string TickerActualizaEscrutado()
+        {
+            return EventBuild("TICKER/CambiaEscrutado", "MAP_INT_PAR", "1", 1);
+        }
+        public string TickerActualizaDatos()
+        {
+            return EventBuild("TICKER/CambiaResultado", "MAP_INT_PAR", "1", 1);
+        }
         public string TickerActualizaDatosIndividualizado(List<PartidoDTO> partidos)
         {
             StringBuilder signal = new StringBuilder();
             foreach (var part in partidos)
             {
-                string cod = string.Equals(part.codigo, "11103") ? "00103" : part.codigo;
-                signal.Append(EventBuild($"TICKER/{cod}/HaCambiado", "MAP_INT_PAR", "1", 1));
+                signal.Append(EventBuild($"TICKER/{part.padre}/HaCambiado", "MAP_INT_PAR", "1", 1));
             }
             return signal.ToString();
         }
@@ -83,45 +133,104 @@ namespace Elecciones_Europeas.src.mensajes.builders
         public string TickerYaNoEstaIndividualizado(List<PartidoDTO> partidos)
         {
             StringBuilder signal = new StringBuilder();
-            List<string> codigos = partidos.Select(par => par.codigo).ToList();
+            List<string> codigos = partidos.Select(par => par.padre).ToList();
             codigos.ForEach(cod =>
             {
-                string code = string.Equals(cod, "11103") ? "00103" : cod;
-                signal.Append(EventBuild($"TICKER/{code}/YaNoEsta", "MAP_INT_PAR", "1", 1));
+                signal.Append(EventBuild($"TICKER/{cod}/YaNoEsta", "MAP_INT_PAR", "1", 1));
             }
                 );
             return signal.ToString();
         }
-        public string TickerActualizaPosiciones() { return EventBuild("TICKER/CambiaOrden", "MAP_INT_PAR", "1", 1); }
-        public string TickerActualizaNumPartidos() { return EventBuild("TICKER/CambiaNPartidos", "MAP_INT_PAR", "1", 1); }
+        public string TickerActualizaPosiciones()
+        {
+            return EventBuild("TICKER/CambiaOrden", "MAP_INT_PAR", "1", 1);
+        }
+        public string TickerActualizaNumPartidos()
+        {
+            return EventBuild("TICKER/CambiaNPartidos", "MAP_INT_PAR", "1", 1);
+        }
 
-        public string TickerVotosEntra(bool oficial) { return oficial ? EventRunBuild("TICKER/VOTOS/ENTRA") : EventRunBuild("TICKER_SONDEO/VOTOS/ENTRA"); }
-        public string TickerVotosSale(bool oficial) { return oficial ? EventRunBuild("TICKER/VOTOS/SALE") : EventRunBuild("TICKER_SONDEO/VOTOS/SALE"); }
-        public string TickerHistoricosEntra(bool oficial) { return oficial ? EventRunBuild("TICKER/HISTORICOS/ENTRA") : EventRunBuild("TICKER_SONDEO/HISTORICOS/ENTRA"); }
-        public string TickerHistoricosSale(bool oficial) { return oficial ? EventRunBuild("TICKER/HISTORICOS/SALE") : EventRunBuild("TICKER_SONDEO/HISTORICOS/SALE"); }
-        public string TickerMillonesEntra() { return EventRunBuild("TICKER/MILLONES/ENTRA"); }
-        public string TickerMillonesSale() { return EventRunBuild("TICKER/MILLONES/SALE"); }
+        public string TickerVotosEntra(bool oficial)
+        {
+            return oficial ? EventRunBuild("TICKER/VOTOS/ENTRA") : EventRunBuild("TICKER_SONDEO/VOTOS/ENTRA");
+        }
+        public string TickerVotosSale(bool oficial)
+        {
+            return oficial ? EventRunBuild("TICKER/VOTOS/SALE") : EventRunBuild("TICKER_SONDEO/VOTOS/SALE");
+        }
+        public string TickerHistoricosEntra(bool oficial)
+        {
+            return oficial ? EventRunBuild("TICKER/HISTORICOS/ENTRA") : EventRunBuild("TICKER_SONDEO/HISTORICOS/ENTRA");
+        }
+        public string TickerHistoricosSale(bool oficial)
+        {
+            return oficial ? EventRunBuild("TICKER/HISTORICOS/SALE") : EventRunBuild("TICKER_SONDEO/HISTORICOS/SALE");
+        }
+        public string TickerMillonesEntra()
+        {
+            return EventRunBuild("TICKER/MILLONES/ENTRA");
+        }
+        public string TickerMillonesSale()
+        {
+            return EventRunBuild("TICKER/MILLONES/SALE");
+        }
 
-        public string TickerFotosEntra() { return EventRunBuild("TICKER/FOTOS/ENTRA"); }
-        public string TickerFotosSale() { return EventRunBuild("TICKER/FOTOS/SALE"); }
+        public string TickerFotosEntra()
+        {
+            return EventRunBuild("TICKER/FOTOS/ENTRA");
+        }
+        public string TickerFotosSale()
+        {
+            return EventRunBuild("TICKER/FOTOS/SALE");
+        }
 
         //PP_PSOE
-        public string PP_PSOEEntra() { return Entra("TICKER/PP_PSOE"); }
-        public string PP_PSOESale() { return Sale("TICKER/PP_PSOE"); }
+        public string PP_PSOEEntra()
+        {
+            return Entra("TICKER/PP_PSOE");
+        }
+        public string PP_PSOESale()
+        {
+            return Sale("TICKER/PP_PSOE");
+        }
 
-        public string PP_PSOEaGenerales() { return EventRunBuild("TICKER/PP_PSOE/A_GENERALES"); }
-        public string PP_PSOEaEuropeas() { return EventRunBuild("TICKER/PP_PSOE/A_EUROPEAS"); }
+        public string PP_PSOEaGenerales()
+        {
+            return EventRunBuild("TICKER/PP_PSOE/A_GENERALES");
+        }
+        public string PP_PSOEaEuropeas()
+        {
+            return EventRunBuild("TICKER/PP_PSOE/A_EUROPEAS");
+        }
 
         //DESPLIEGAS
-        public string Despliega4() { return EventRunBuild("TICKER/DESPLIEGO_4"); }
-        public string Despliega5() { return EventRunBuild("TICKER/DESPLIEGO_5"); }
+        public string Despliega4()
+        {
+            return EventRunBuild("TICKER/DESPLIEGO_4");
+        }
+        public string Despliega5()
+        {
+            return EventRunBuild("TICKER/DESPLIEGO_5");
+        }
 
-        public string RecuperaTodos() { return EventRunBuild("TICKER/RECUPERO_TODOS"); }
+        public string RecuperaTodos()
+        {
+            return EventRunBuild("TICKER/RECUPERO_TODOS");
+        }
 
         //PACTOMETRO
-        public string pactosEntra() { return Entra("PACTOMETRO"); }
-        public string pactosReinicio() { return EventRunBuild("PACTOMETRO/INICIO"); }
-        public string pactosSale() { return Sale("PACTOMETRO"); }
+        public string pactosEntra()
+        {
+            return Entra("PACTOMETRO");
+        }
+        public string pactosReinicio()
+        {
+            return EventRunBuild("PACTOMETRO/INICIO");
+        }
+        public string pactosSale()
+        {
+            return Sale("PACTOMETRO");
+        }
 
         public string pactosEntraDerecha(int posicionPartido)
         {
@@ -154,9 +263,18 @@ namespace Elecciones_Europeas.src.mensajes.builders
         }
 
         //INDEPENDENTISMO
-        public string independentismoEntra() { return Entra("PACTOMETRO_IND"); }
-        public string independentismoReinicio() { return EventRunBuild("PACTOMETRO_IND/INICIO"); }
-        public string independentismoSale() { return Sale("PACTOMETRO_IND"); }
+        public string independentismoEntra()
+        {
+            return Entra("PACTOMETRO_IND");
+        }
+        public string independentismoReinicio()
+        {
+            return EventRunBuild("PACTOMETRO_IND/INICIO");
+        }
+        public string independentismoSale()
+        {
+            return Sale("PACTOMETRO_IND");
+        }
 
         public string independentismoEntraDerecha(int posicionPartido)
         {
@@ -230,40 +348,103 @@ namespace Elecciones_Europeas.src.mensajes.builders
 
         //CARTONES
         //PARTICIPACION
-        public string participacionEntra() { return Entra("PARTICIPACION"); }
-        public string participacionEncadena() { return EventRunBuild("PARTICIPACION/CAMBIA"); }
-        public string participacionSale() { return Sale("PARTICIPACION"); }
+        public string participacionEntra()
+        {
+            return Entra("PARTICIPACION");
+        }
+        public string participacionEncadena()
+        {
+            return EventRunBuild("PARTICIPACION/CAMBIA");
+        }
+        public string participacionSale()
+        {
+            return Sale("PARTICIPACION");
+        }
 
         //CCAA
-        public string ccaaEntra() { return Entra("CCAA"); }
-        public string ccaaEncadena() { return EventRunBuild("CCAA/CAMBIA"); }
-        public string ccaaSale() { return Sale("CCAA"); }
+        public string ccaaEntra()
+        {
+            return Entra("CCAA");
+        }
+        public string ccaaEncadena()
+        {
+            return EventRunBuild("CCAA/CAMBIA");
+        }
+        public string ccaaSale()
+        {
+            return Sale("CCAA");
+        }
 
         //FICHAS DE PARTIDO
-        public string fichaEntra() { return Entra("CARTONES"); }
-        public string fichaEncadena() { return EventRunBuild("CARTONES/CAMBIA"); }
-        public string fichaSale() { return Sale("CARTONES"); }
+        public string fichaEntra()
+        {
+            return Entra("CARTONES");
+        }
+        public string fichaEncadena()
+        {
+            return EventRunBuild("CARTONES/CAMBIA");
+        }
+        public string fichaSale()
+        {
+            return Sale("CARTONES");
+        }
 
         //PACTOMETRO
-        public string pactometroEntra() { return Entra("PACTOMETRO"); }
-        public string pactometroEncadena() { return EventRunBuild("PACTOMETRO/POSICIONES"); }
-        public string pactometroSale() { return Sale("PACTOMETRO"); }
+        public string pactometroEntra()
+        {
+            return Entra("PACTOMETRO");
+        }
+        public string pactometroEncadena()
+        {
+            return EventRunBuild("PACTOMETRO/POSICIONES");
+        }
+        public string pactometroSale()
+        {
+            return Sale("PACTOMETRO");
+        }
 
-        public string pactometroVictoria() { return EventRunBuild("PACTOMETRO/VICTORIA"); }
+        public string pactometroVictoria()
+        {
+            return EventRunBuild("PACTOMETRO/VICTORIA");
+        }
 
         //MAYORIAS
-        public string mayoriasEntra() { return Entra("MAYORIAS"); }
-        public string mayoriasEncadena() { return EventRunBuild("MAYORIAS/CAMBIA"); }
-        public string mayoriasSale() { return Sale("MAYORIAS"); }
+        public string mayoriasEntra()
+        {
+            return Entra("MAYORIAS");
+        }
+        public string mayoriasEncadena()
+        {
+            return EventRunBuild("MAYORIAS/CAMBIA");
+        }
+        public string mayoriasSale()
+        {
+            return Sale("MAYORIAS");
+        }
 
         //SUPERFALDON
-        public string superfaldonEntra() { return Entra("SUPERFALDON"); }
-        public string superfaldonSale() { return Sale("SUPERFALDON"); }
+        public string superfaldonEntra()
+        {
+            return Entra("SUPERFALDON");
+        }
+        public string superfaldonSale()
+        {
+            return Sale("SUPERFALDON");
+        }
 
         //SEDES
-        public string superfaldonSedesEntra() { return Entra("SEDES"); }
-        public string superfaldonSedesEncadena() { return Entra("SEDES/ENCADENA"); }
-        public string superfaldonSedesSale() { return Sale("SEDES"); }
+        public string superfaldonSedesEntra()
+        {
+            return Entra("SEDES");
+        }
+        public string superfaldonSedesEncadena()
+        {
+            return Entra("SEDES/ENCADENA");
+        }
+        public string superfaldonSedesSale()
+        {
+            return Sale("SEDES");
+        }
 
 
         //CONSTRUCTORES

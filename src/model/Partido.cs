c03d233a5f878e38e858086f53432701f2c2e020
Mario <ggperpi@gmail.com>
2025-12-19 13:27:17 +0100

    Sin padre

--- src/model/Partido.cs
+++ src/model/Partido.cs
@@ -9,7 +9,13 @@ namespace Elecciones_Europeas.src.model.IPF
     public class Partido
     {
         public string codigo { get; set; }
-        public string codigoPadre { get; set; }
+        public string codigoPadre
+        {
+            get => codigo;
+            set
+            { /* intentionally ignored - codigo is the source of truth */
+            }
+        }
         public string siglas { get; set; }
         public string nombre { get; set; }
         public string candidato { get; set; }
@@ -25,7 +31,8 @@ namespace Elecciones_Europeas.src.model.IPF
         {
             configuration = ConfigManager.GetInstance();
             this.codigo = codigo;
-            this.codigoPadre = codigoPadre;
+            // codigoPadre parameter is accepted for compatibility but ignored:
+            // codigoPadre will always be represented by this.codigo
             this.siglas = siglas;
             this.nombre = nombre;
             this.candidato = candidato;
@@ -33,6 +40,7 @@ namespace Elecciones_Europeas.src.model.IPF
         }
         public override string ToString()
         {
+            // codigoPadre returns codigo, so this prints codigo for both fields
             return $"{codigo};{codigoPadre};{siglas};{nombre};{candidato};{independentismo}";
         }
         public async Task ToJson()
@@ -42,12 +50,12 @@ namespace Elecciones_Europeas.src.model.IPF
             string json = JsonSerializer.Serialize(this, options);
             await File.WriteAllTextAsync(fileName, json);
         }
+
         public async Task ToCsv()
         {
             string fileName = $"{configuration.GetValue("rutaArchivos")}\\CSV\\Partido.csv";
             string csv = $"Codigo;Siglas;Codigo Padre;Nombre;Candidato;Tendencia\n{this.ToString()}";
             await File.WriteAllTextAsync(fileName, csv);
-
         }
     }
 }
